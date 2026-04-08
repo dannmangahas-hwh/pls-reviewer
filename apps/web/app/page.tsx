c6a6550c986a622d5e266697464b030523053605
angelovelasco-hungryworkhorse <angelo.velasco@hungryworkhorse.com>
2026-04-08 18:01:21 +0800

    feat: add What You Gain section to website

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "@/components/hero"
 import { Challenges } from "@/components/challenges"
 import { CTASection } from "@/components/cta-section"
+import { WhatYouGain } from "@/components/what-you-gain";
 
 export default function Page() {
   return (
     <>
       <Hero />
       <Challenges />
+      <WhatYouGain />
       <CTASection />
     </>
   )
